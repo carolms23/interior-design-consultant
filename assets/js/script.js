38b3798a4f490e826283d72059e9d0e525c38fba
carolms23 <carolinems360@gmail.com>
2022-03-18 14:36:28 -0300

    alteraçoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 function mobileMenu(){
-    if (document.getElementById("menu-lateral").style.display == "block"){
+    if (document.getElementById("menu-lateral").style.display == "flex"){
         document.getElementById("menu-lateral").style.display = "none";
     }else{
-        document.getElementById("menu-lateral").style.display = "block"
+        document.getElementById("menu-lateral").style.display = "flex"
     }
 }
 document.getElementById("menu-icon").addEventListener("click",mobileMenu)
+document.getElementById("xmark").addEventListener("click",mobileMenu)
